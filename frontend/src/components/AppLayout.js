e4da16f24702b695f5e39f15fa21a3c152c0ac7d
Jimin Kim <wweww1596@likelion.org>
2022-08-25 17:54:47 +0900

    CSS Grid와 Ant Design을 활용한 레이아웃

---
+++ frontend/src/components/AppLayout.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Input, Menu } from "antd";
+import "./AppLayout.scss";
+import SuggestionList from "./SuggestionList";
+import StoryList from "./StoryList";
+import LogoImage from "assets/logo.png"
+
+
+function AppLayout({ children }) {
+    return (
+        <div className="app">
+            <div className="header">
+                <h1 className="page-title">
+                    <img src={LogoImage} alt="logo" />
+                </h1>
+                <div className="search">
+                <Input.Search />
+            </div>
+            <div className="topnav">
+                <Menu mode="horizontal">
+                    <Menu.Item>메뉴1</Menu.Item>
+                    <Menu.Item>메뉴2</Menu.Item>
+                    <Menu.Item>메뉴3</Menu.Item>
+                </Menu>
+            </div>
+            </div> 
+            <div className="contents">{children}</div>
+            <div className="sidebar">
+                <StoryList style={{ marginBottom: "1rem" }} />
+                <SuggestionList />
+            </div>
+            <div className="footer">
+                &copy; 2022. Jimin Kim
+            </div>
+        </div>
+    );
+}
+
+export default AppLayout;
